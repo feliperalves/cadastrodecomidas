c240d15f083faaf57a30ef28d7969f68f4120d47
feliperalves <feliperchalves@gmail.com>
2023-05-02 14:23:49 -0300

    Feito

--- form-edit.php
+++ form-edit.php
@@ -3,10 +3,9 @@ require 'init.php';
 
 $id = isset($_GET['id']) ? (int) $_GET['id'] : null;
 
-if (empty($id))
-{
-    echo "ID para alteração não definido";
-    exit;
+if (empty($id)) {
+	echo "ID para alteração não definido";
+	exit;
 }
 
 $PDO = db_connect();
@@ -16,53 +15,97 @@ $stmt->bindParam(':id', $id, PDO::PARAM_INT);
 $stmt->execute();
 $pratos = $stmt->fetch(PDO::FETCH_ASSOC);
 
-if (!is_array($pratos))
-{
-    echo "Nenhum prato encontrado";
+if (!is_array($pratos)) {
+	echo "Nenhum prato encontrado";
 }
 ?>
 </doctype HTML>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<title>Cadastro de Comidas Favoritas</title>
-		<link rel="stylesheet" href="estilo.css">
-		<link href="https://fonts.googleapis.com/css2?family=ABeeZee&display=swap" rel="stylesheet">
-		<link href = "bootstrap/css/bootstrap.css" rel="stylesheet">
-        <script src = "bootstrap/js/bootstrap.js"></script>
-	</head>
-	<body>
+
+<head>
+	<meta charset="utf-8">
+	<title>Cadastro de Comidas Favoritas</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+	<link href="bootstrap/css/bootstrap.css" rel="stylesheet" />
+	<link rel="stylesheet" href="estilo.css">
+	<link href="https://fonts.googleapis.com/css2?family=ABeeZee&display=swap" rel="stylesheet">
+	<script src="bootstrap/js/popper.min.js"></script>
+	<script src="bootstrap/js/bootstrap.js"></script>
+	<script src="bootstrap/js/jquery.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
 	<div class="container-fluid">
-		<header class="row" id="header">
-			<a href="index.php" id="a-header">
-				<h1 id="h1-header">Cadastro de Comidas Favoritas</h1>
-			</a>
-		</header>
+		<nav class="navbar navbar-expand bg-white nav-justified ">
+			<div class="container-fluid justify-content-center align-items-center">
+				<ul class="navbar-nav ">
+					<a class="navbar-brand" href="index.php">
+						<span class="navbar-text" style="color: orange;"> Cadastro de Comidas Favoritas</span>
+					</a>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#"
+							style="color: orange;">Pratos</a>
+						<ul class="dropdown-menu">
+							<li>
+								<a href="form-add.php" class="dropdown-item">Cadastrar Prato</a>
+							</li>
+							<li>
+								<a href="form-edit-navbar.php" class="dropdown-item">Editar Prato</a>
+							</li>
+							<li>
+								<a href="form-remove-navbar.php" class="dropdown-item">Remover Prato</a>
+							</li>
+						</ul>
+					</li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#"
+							style="color: orange;">Ingredientes</a>
+						<ul class="dropdown-menu">
+							<li>
+								<a href="#" class="dropdown-item">Em breve</a>
+							</li>
+							<li>
+								<a href="#" class="dropdown-item">Em Breve</a>
+							</li>
+							<li>
+								<a href="#" class="dropdown-item">Em Breve</a>
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+		</nav>
 	</div>
-		<div class="container my-5">
-		<div class="card p-3 mx-auto border border-danger" style="max-width: 500px;">
-			<h2 class="text-center text-danger">Edite sua comida favorita</h2>
+	<div class="container my-5">
+		<div class="card p-3 mx-auto border border-warning" style="max-width: 500px;">
+			<h2 class="text-center text-warning">Edite sua comida favorita</h2>
 			<form action="edit.php" method="post">
 				<div class="form-group">
-					<label for="nome">Nome: </label>
-					<input type="text" class="form-control col-sm border-danger" name="nome" id="nome"  value="<?php echo $pratos["nome"] ?>">
+					<label for="nome" style="color: orange;">Nome: </label>
+					<input type="text" class="form-control col-sm border-warning" name="nome" id="nome"
+						value="<?php echo $pratos["nome"] ?>">
 				</div>
 				<div class="form-group">
-					<label for="ingredientes">Ingredientes: </label>
-					<input type="text" class="form-control col-sm border-danger"  name="ingredientes" id = "ingredientes"  value = "<?php echo $pratos["ingredientes"] ?>">
+					<label for="ingredientes" style="color: orange;">Ingredientes: </label>
+					<input type="text" class="form-control col-sm border-warning" name="ingredientes" id="ingredientes"
+						value="<?php echo $pratos["ingredientes"] ?>">
 				</div>
 				<div class="form-group">
-					<label for="dificuldade_preparo">Dificuldade de preparo: </label>
-					<input type="text" class="form-control col-sm border-danger" name="dificuldade_preparo" id="dificuldade_preparo"  value="<?php echo $pratos["dificuldade_preparo"] ?>">
+					<label for="dificuldade_preparo" style="color: orange;">Dificuldade de preparo: </label>
+					<input type="text" class="form-control col-sm border-warning" name="dificuldade_preparo"
+						id="dificuldade_preparo" value="<?php echo $pratos["dificuldade_preparo"] ?>">
 				</div>
 				<div class="form-group">
-					<label for="quanto_gosta">Quanto Gosta: </label>
-					<input type="text" class="form-control col-sm border-danger" name="quanto_gosta" id = "quanto_gosta"  value="<?php echo $pratos["quanto_gosta"] ?>">
+					<label for="quanto_gosta" style="color: orange;">Quanto Gosta: </label>
+					<input type="text" class="form-control col-sm border-warning" name="quanto_gosta" id="quanto_gosta"
+						value="<?php echo $pratos["quanto_gosta"] ?>">
 				</div>
-					<input type = "hidden" name = "id" value="<?php echo $id ?>">
-					<button type="submit" class="btn btn-danger">Editar</button>
+				<input type="hidden" name="id" value="<?php echo $id ?>">
+				<button type="submit" class="btn btn-warning" style = "color: white;">Editar</button>
 			</form>
 		</div>
 	</div>
-		</body>
+</body>
+
 </html>
--- init.php
+++ init.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
+define('DB_USER','alunos');
+define('DB_PASS','cefetmg');
 define('DB_NAME','crudwebii');
 
 date_default_timezone_set('America/Sao_Paulo');
